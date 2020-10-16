f467c2b919ad3910103c0e1f52dc54b4afd45b8e
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-10-16 14:02:30 +0300

    Add files via upload

---
+++ Easy/Staircase/python/main.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/staircase#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(n):
+        print(" " * (n - i - 1), end="")
+        print("#" * (i + 1))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
