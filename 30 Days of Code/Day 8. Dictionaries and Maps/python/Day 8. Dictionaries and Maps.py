bc12754b048ba3b7af63cb94dc9a1831f856ed69
qqlexa <37111337+qqlexa@users.noreply.github.com>
2020-11-05 17:17:23 +0200

    Add files via upload

--- 30 Days of Code/Day 8. Dictionaries and Maps/python/Day 8. Dictionaries and Maps.py
+++ 30 Days of Code/Day 8. Dictionaries and Maps/python/Day 8. Dictionaries and Maps.py
@@ -1,1 +1,18 @@
-s
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+phone_book = dict()
+for _ in range(n):
+    name, number = input().split()
+    phone_book[name] = number
+
+name = input()
+while name:
+    try:
+        print(f"{name}={phone_book[name]}")
+    except:
+        print("Not found")
+        
+    try:
+        name = input()
+    except EOFError:
+        break
